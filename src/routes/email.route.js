04f1c03c217efb78631804e0205a6d9edcb3cc26
Kartik Mishra <kartik26nov2004@gmail.com>
2025-09-17 01:09:19 +0530

    gateway auth.

--- src/routes/email.route.js
+++ src/routes/email.route.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { verificationEmail } from "../controllers/email.controller.js";
+import { gatewayAuth } from '../middlewares/gatewayAuth.js'
 
 const router = Router();
 
-router.post("/verification", verificationEmail);
+router.post("/verification", gatewayAuth, verificationEmail);
 
 export default router;
